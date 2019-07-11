587a7e2d45e0a7573a33197cf529a2eade29a43f
lance.zhang(张澜潇) <lance.zhang@xgimi.com>
2019-07-11 17:30:03 +0800

    project intit

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// @指的是src目录
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
 
+// 当用户访问根路径时，我给用户展示的是Home组件
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
