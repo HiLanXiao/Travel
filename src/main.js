e7e4de4e947d0d8297ef9e4a7e8d6488063c2a58
lance.zhang(张澜潇) <lance.zhang@xgimi.com>
2019-07-13 11:39:59 +0800

    change

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 
 // 它会帮你去匹配后缀如：App.vue
 import App from './App'
@@ -9,9 +10,11 @@ import router from './router'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper /* { default global options } */)
 
 /* eslint-disable no-new */
 // 根实例，挂载到index.html上的id=app的div元素
---
+++ src/pages/home/components/HomeSwiper.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class='wrapper'>
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList"
+                    :key="item.id"><img class="swiper-image"
+             :src="item.imgUrl"
+             alt="">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"
+           slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [
+        {
+          id: '0001',
+          imgUrl: 'http://m.elongstatic.com/promotions/index/fold/fold1/pic/suzhou.jpg'
+        },
+        {
+          id: '0002',
+          imgUrl: 'http://m.elongstatic.com/promotions/index/fold/fold1/pic/hangzhou.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+  background #fff !important
+.swiper-image
+  width 100%
+.wrapper
+  overflow hidden
+  width 100%
+  height 0
+  padding-bottom 31.25% // 高度会相对于宽度撑开31.25%的空间
+  .swiper-img
+    width 100%
+</style>
