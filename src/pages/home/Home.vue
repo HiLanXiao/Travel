5b6330dc41e2c6f15ec7d7c51d2b2b1188477bd9
lance.zhang(张澜潇) <lance.zhang@xgimi.com>
2019-07-13 17:57:36 +0800

    today

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import HomeHeader from './components/HomeHeader'
 import HomeSwiper from './components/HomeSwiper'
 import HomeIcons from './components/HomeIcons'
+import HomeRecommend from './components/HomeRecommend'
 
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   }
 }
 </script>
---
+++ src/pages/home/components/HomeRecommend.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="recommend-title">热销推荐</div>
+    <ul>
+      <li class="item">
+        <div class="item-img-wrapper">
+          <img class="item-img"
+               src=""
+               alt="">
+        </div>
+        <div class="item-info">
+          <p></p>
+          <p></p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+.recommend-title
+  line-height 0.4rem
+  background #ccc
+  text-indent 0.2rem
+</style>
